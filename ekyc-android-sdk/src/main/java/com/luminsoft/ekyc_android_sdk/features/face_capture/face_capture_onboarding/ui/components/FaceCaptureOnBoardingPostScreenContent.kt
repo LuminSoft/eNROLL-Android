72cd30529bb6078fbaea930af0eddf7fa8402bb1
Andrew Samir <andrew_samir93@hotmail.com>
2024-03-18 09:03:59 +0200

    start animation

--- ekyc-android-sdk/src/main/java/com/luminsoft/ekyc_android_sdk/features/face_capture/face_capture_onboarding/ui/components/FaceCaptureOnBoardingPostScreenContent.kt
+++ ekyc-android-sdk/src/main/java/com/luminsoft/ekyc_android_sdk/features/face_capture/face_capture_onboarding/ui/components/FaceCaptureOnBoardingPostScreenContent.kt
@@ -6,22 +6,54 @@ import androidx.activity.compose.ManagedActivityResultLauncher
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.VectorConverter
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.animateIntOffset
+import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.airbnb.lottie.model.content.CircleShape
 import com.luminsoft.ekyc_android_sdk.R
 import com.luminsoft.ekyc_android_sdk.core.failures.AuthFailure
 import com.luminsoft.ekyc_android_sdk.core.models.EKYCFailedModel
@@ -42,6 +74,7 @@ import com.luminsoft.ekyc_android_sdk.ui_components.components.ButtonView
 import com.luminsoft.ekyc_android_sdk.ui_components.components.DialogView
 import com.luminsoft.ekyc_android_sdk.ui_components.components.SpinKitLoadingIndicator
 import org.koin.compose.koinInject
+import kotlin.math.roundToInt
 
 
 @Composable
@@ -59,6 +92,7 @@ fun FaceCaptureBoardingPostScanScreenContent(
 
     val faceCaptureUseCase = FaceCaptureUseCase(koinInject())
     val selfieImageApproveUseCase = SelfieImageApproveUseCase(koinInject())
+
 
     val faceCaptureOnBoardingPostScanViewModel =
         document.value?.let {
@@ -93,6 +127,7 @@ fun FaceCaptureBoardingPostScanScreenContent(
             }
         }
 
+//    GameCharacterMovement()
     MainContent(
         navController,
         onBoardingViewModel,
@@ -100,6 +135,55 @@ fun FaceCaptureBoardingPostScanScreenContent(
         startForResult,
         faceCaptureOnBoardingPostScanViewModel!!
     )
+}
+
+@Composable
+fun GameCharacterMovement() {
+
+    val startPosition = Offset(50f, 100f)
+    val endPosition = Offset(200f, 100f)
+    val position = remember { Animatable(startPosition, Offset.VectorConverter) }
+
+    val startPosition1 = Offset(350f, 100f)
+    val position1 = remember { Animatable(startPosition1, Offset.VectorConverter) }
+
+    LaunchedEffect(endPosition) {
+        position.animateTo(
+            targetValue = endPosition,
+            animationSpec = keyframes {
+                durationMillis = 5000
+                //                controlPoint.value at 2500 // midway point controlled by the draggable control point
+            }
+        )
+    }
+    LaunchedEffect(endPosition) {
+        position1.animateTo(
+            targetValue = endPosition,
+            animationSpec = keyframes {
+                durationMillis = 5000
+            }
+        )
+    }
+
+
+    Box(modifier = Modifier.fillMaxSize()) {
+
+        Row {
+            Icon(
+                Icons.Filled.Face, contentDescription = "Localized description", modifier = Modifier
+                    .size(50.dp)
+                    .offset(x = position.value.x.dp, y = position.value.y.dp)
+            )
+            Icon(
+                Icons.Filled.Face, contentDescription = "Localized description", modifier = Modifier
+                    .size(50.dp)
+                    .offset(x = position1.value.x.dp, y = position1.value.y.dp)
+            )
+        }
+
+    }
+
+
 }
 
 @Composable
@@ -217,5 +301,7 @@ private fun MainContent(
 
         }
     }
+
+
 }
 
