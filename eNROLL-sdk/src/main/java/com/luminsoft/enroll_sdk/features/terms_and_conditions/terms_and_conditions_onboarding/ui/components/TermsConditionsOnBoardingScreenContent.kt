83d16a6fd7c02f88751472bec6be8e647e2cf65d
Ahmed Radwan <Radwan@Ahmeds-MacBook-Pro.local>
2024-08-06 09:26:59 +0300

    editing the ui to be the same as ios version

--- eNROLL-sdk/src/main/java/com/luminsoft/enroll_sdk/features/terms_and_conditions/terms_and_conditions_onboarding/ui/components/TermsConditionsOnBoardingScreenContent.kt
+++ eNROLL-sdk/src/main/java/com/luminsoft/enroll_sdk/features/terms_and_conditions/terms_and_conditions_onboarding/ui/components/TermsConditionsOnBoardingScreenContent.kt
@@ -16,10 +16,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -29,7 +32,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
@@ -89,7 +91,7 @@ fun TermsConditionsOnBoardingScreenContent(
 
 
 
-    BackGroundView(navController = navController, showAppBar = true) {
+    BackGroundView(navController = navController, showAppBar = false) {
         if (termsAccepted.value) {
 
             val isEmpty =
@@ -179,7 +181,7 @@ fun PdfViewerWidget(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp),
-        contentAlignment = Alignment.TopCenter
+        contentAlignment = Alignment.Center
     ) {
         Column(
             modifier = Modifier
@@ -188,15 +190,23 @@ fun PdfViewerWidget(
         ) {
             Text(
                 text = stringResource(id = R.string.readTermsAndConditions),
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color.Black,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.SemiBold,
+                color = primary,
                 textAlign = TextAlign.Center,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(bottom = 16.dp)
+                    .padding(bottom = 10.dp)
             )
 
+            Divider(
+                color = MaterialTheme.colorScheme.primary,
+                thickness = 4.dp,
+                modifier = Modifier.width(50.dp).align(Alignment.CenterHorizontally)
+            )
+
+
+            Spacer(modifier = Modifier.height(16.dp))
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -230,10 +240,15 @@ fun PdfViewerWidget(
                     title = stringResource(id = R.string.acceptTermsAndConditions)
                 )
                 Spacer(modifier = Modifier.height(8.dp))
+
                 ButtonView(
                     onClick = { showConfirmationDialog = true },
-                    title = stringResource(id = R.string.exit)
+                    title = stringResource(id = R.string.exit),
+                    textColor = MaterialTheme.colorScheme.primary,
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    borderColor = MaterialTheme.colorScheme.primary,
                 )
+
 
             }
         }
