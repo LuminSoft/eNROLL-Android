145fddb00d7e0ddab7f2d38eef7fd03e192d6d75
luminsoft <andrew_samir93@hotmail.com>
2025-06-15 18:11:15 +0300

    version = '1.3.2'

--- eNROLL-sdk/src/main/java/com/luminsoft/enroll_sdk/core/sdk/EnrollSDK.kt
+++ eNROLL-sdk/src/main/java/com/luminsoft/enroll_sdk/core/sdk/EnrollSDK.kt
@@ -30,32 +30,25 @@ object EnrollSDK {
     var enrollForcedDocumentType: EnrollForcedDocumentType? =
         EnrollForcedDocumentType.NATIONAL_ID_OR_PASSPORT
 
-    var isLuminDomain = true
-    private fun getBaseUrl(): String {
-        return when (environment) {
-            EnrollEnvironment.STAGING -> "https://enrollstg.nasps.org.eg"
-            EnrollEnvironment.PRODUCTION -> "https://enroll.nasps.org.eg"
-        }
-    }
 
     private fun getLuminBaseUrl(): String {
         return when (environment) {
             EnrollEnvironment.STAGING -> "https://enrollstg.luminsoft.net"
-            EnrollEnvironment.PRODUCTION -> "https://enroll.luminsoft.net"
+            EnrollEnvironment.PRODUCTION -> "https://enrollgateway.luminsoft.net"
         }
     }
 
     fun getApisUrl(): String {
-        return if (isLuminDomain)
-            getLuminBaseUrl() + ":7400/OnBoarding/"
-        else
-            getBaseUrl() + ":7400/OnBoarding/"
+        return when (environment) {
+            EnrollEnvironment.STAGING -> getLuminBaseUrl() + ":7400/OnBoarding/"
+            EnrollEnvironment.PRODUCTION -> getLuminBaseUrl() + ":443/OnBoarding/"
+        }
     }
 
     fun getImageUrl(): String {
-        return if (isLuminDomain)
-            getLuminBaseUrl() + ":7400/OnBoarding/api/v1/onboarding/Image/GetNationalIdPhotoImage"
-        else
-            getBaseUrl() + ":7400/OnBoarding/api/v1/onboarding/Image/GetNationalIdPhotoImage"
+        return when (environment) {
+            EnrollEnvironment.STAGING -> getLuminBaseUrl() + ":7400/OnBoarding/api/v1/onboarding/Image/GetNationalIdPhotoImage"
+            EnrollEnvironment.PRODUCTION -> getLuminBaseUrl() + ":443/OnBoarding/api/v1/onboarding/Image/GetNationalIdPhotoImage"
+        }
     }
 }
