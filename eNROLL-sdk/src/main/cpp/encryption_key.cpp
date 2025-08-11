cb1ccec1797fefef7ec69ec5fad794f75ecf5f6c
luminsoft <andrew_samir93@hotmail.com>
2025-08-11 15:28:19 +0300

    merge secure apis

--- eNROLL-sdk/src/main/cpp/encryption_key.cpp
+++ eNROLL-sdk/src/main/cpp/encryption_key.cpp
@@ -5,7 +5,7 @@ extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_luminsoft_enroll_1sdk_core_utils_EncryptionHelper_getNativeKey(
         JNIEnv *env,
-        jobject /* this */) {
+        jobject) {
     std::string part1 = "4D9f6H8";
     std::string part2 = "k2L3p0";
     std::string part3 = "Z1a";
--- eNROLL-sdk/src/main/java/com/luminsoft/enroll_sdk/main_forget_profile_data/main_forget_presentation/common/ForgetListScreenContent.kt
+++ eNROLL-sdk/src/main/java/com/luminsoft/enroll_sdk/main_forget_profile_data/main_forget_presentation/common/ForgetListScreenContent.kt
@@ -17,7 +17,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
